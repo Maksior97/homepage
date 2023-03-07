d73fcc9ca07e4e944579bf02a7c1465eb27178b4
Maksymilian Chrapkowicz <maksymilian.chrapkowicz@gmail.com>
2023-03-07 13:19:52 +0100

    changed name of button to use it

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ let touch = document.querySelector(".touch");
 let photo = document.querySelector(".photo");
 let remove = document.querySelector(".remove");
 let source = document.querySelector(".source");
-let button = document.querySelector(".button");
+let button = document.querySelector(".section__button");
 let body = document.querySelector(".body");
-let choice = document.querySelector(".choice");
+let choice = document.querySelector(".js-choice");
 
 touch.addEventListener("click", () => {
     photo.remove();
